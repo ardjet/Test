2f0c7b3be6cda7ee4933658636690ff79d396ee5
ardjet <dadada124@gmail.com>
2012-04-16 22:35:05 +0900

    ドットインストール　文字オブジェクト

---
+++ langtest.rb
@@ -0,0 +1,21 @@
+# encoding: sjis
+
+# 文字列オブジェクト
+
+x = "hello"
+y = 'hello'
+
+#puts "hello\nworld"
+
+name = "yousuke"
+#puts "my name is #{name}"
+#puts "こんにちは"
+
+#p "hello".include?("h")
+
+s = "hello"
+s1 = s.upcase!
+
+p s
+p s1
+
---
+++ num.rb
@@ -0,0 +1,9 @@
+#x = 20.3
+#x = Rational(2,3) + Rational(5,6)
+x = 5 % 4 
+
+p x
+
+p 10.5.round
+
+p rand(30)
